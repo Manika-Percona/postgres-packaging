5062bc70e7acd52bed709fbe2dee27d138a1461d
Manika Singhal <manika.singhal@percona.com>
2025-09-11 11:34:11 +0530

    fix

--- versions.sh
+++ versions.sh
@@ -75,7 +75,7 @@ case "$1" in
         PG_CRON_SRC_BRANCH="v${PG_CRON_VERSION}"
         PG_CRON_RPM_RELEASE='2'
         PG_CRON_DEB_RELEASE='2'
-        PG_CRON_RELEASE='2'
+        PG_CRON_RELEASE='1'
 
         # urls
         PG_CRON_SRC_REPO="https://github.com/citusdata/pg_cron.git"
