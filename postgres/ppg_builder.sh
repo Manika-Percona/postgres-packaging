e939450072241587308d1bb55392d1022b6e5f61
Manika Singhal <manika.singhal@percona.com>
2025-09-16 17:43:44 +0530

    fix paths

--- postgres/ppg_builder.sh
+++ postgres/ppg_builder.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -xe
 # Versions and other variables
-source ../versions.sh "postgresql"
+source versions.sh "postgresql"
 # Common functions
-source ../common-functions.sh
+source common-functions.sh
 
 get_sources(){
     cd "${WORKDIR}"
@@ -337,7 +337,7 @@ get_system
 if [ $INSTALL = 0 ]; then
     echo "Dependencies will not be installed"
 else
-    source ../install-deps.sh "postgresql"
+    source install-deps.sh "postgresql"
 fi
 
 get_sources
