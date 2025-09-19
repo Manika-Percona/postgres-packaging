b9eace691094a998e6610dd01e0fc7c16639d20e
Manika Singhal <manika.singhal@percona.com>
2025-09-19 16:56:11 +0530

    fix

--- pgaudit/pgaudit_builder.sh
+++ pgaudit/pgaudit_builder.sh
@@ -46,10 +46,10 @@ get_sources(){
     wget ${PKG_RAW_URL}/pgaudit/control.in
     wget ${PKG_RAW_URL}/pgaudit/all.patch
     wget ${PKG_RAW_URL}/pgaudit/rules
-    mkdir -p debian/patches
-    mv all.patch debian/patches/
+    #mkdir -p debian/patches
+    #mv all.patch debian/patches/
     rm -rf debian/control*
-    echo "all.patch" > debian/patches/series
+    #echo "all.patch" > debian/patches/series
     #echo "alternative_regression_outputs.patch" >> debian/patches/series
     mv control* debian/
     mv rules debian/
